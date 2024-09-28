a44395a19ed380051cbfbb14dc415bfe4df56756
Aniket Dekate <aniketdekate1@gmail.com>
2024-09-28 23:18:46 +0530

    remove unnecessary component of server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 const app = express()
 const port = 3000;
@@ -7,10 +5,3 @@ app.listen(port, function () {
     console.log(` web server listening on port ${port}`)
     
   })
-=======
-console.log("running");
->>>>>>> 64e6eddf87efe38701b89978dc9c0d9def1089ad
-=======
-console.log("running");
-console.log("ani")
->>>>>>> 125b1975a1553448001121d33e55997948d475ac
