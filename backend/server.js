675a98ec064c679746c20f0ccad6aa810b13b7ee
Aniket Dekate <aniketdekate1@gmail.com>
2024-09-29 00:07:44 +0530

    add atlas at server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,26 @@
+import 'dotenv/config'
+import mongoose from 'mongoose';
 import express from "express";
+
+
+const DB_URL = process.env.MONGO_ATLAS;
+
+
 const app = express();
 const port = 3000;
+
+
+main().then(()=>{
+  console.log("connected to dataBase");
+}).catch((err) =>{
+  console.log(err);
+});
+
+
+async function main() {
+  await mongoose.connect(DB_URL);
+};
+
 
 app.get("/",(req,res)=>{
   res.send("reached");
