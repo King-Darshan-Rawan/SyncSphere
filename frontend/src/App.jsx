1af07a7c634414a807334fae979c3e78d8cd89e8
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-09-29 00:05:14 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,6 @@ function App() {
       <h1 className='text-white'>
       Hello world!
     </h1>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
