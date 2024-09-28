5a920108786f5b78e66953b10a4036cb474e9bf7
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-09-29 01:24:28 +0530

    saved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./../Components/Navbar";
-import Home from "./../Pages/Home";
-import About from "./../Pages/About";
-import Services from "./../Pages/Services";
-import Contact from "./../Pages/Contact";
 import './App.css'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Contact from "./Pages/Contact";
+
 function App() {
   return (
     <Router>
       <Navbar />
-      <h1 className='text-white'>
-      Hello world!
+      <h1 className='text-xs h-1'>
+      mdlkevmkedfn
     </h1>
       <Routes>
         <Route path="/" element={<Home />} />
