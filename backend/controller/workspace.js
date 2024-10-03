b0948474597d162e774ad2713ff1445b6c036c94
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-10-03 19:03:18 +0530

    updated

--- backend/controller/workspace.js
+++ backend/controller/workspace.js
@@ -2,18 +2,18 @@ import { Workspace } from "../models/workspace.js";
 import { User } from "../models/user.js";
 
 let Createworkspace = async (req, res) => {
-  const { UserId, UserName, userPass } = req.body;
+  const { owner , wsname, wspassword } = req.body;
 
   try {
-    const UserF = await User.findById(UserId);
+    const UserF = await User.findById(owner );
     if (!UserF) {
       return res.status(404).json({ msg: "Owner not found" });
     }
     const newWorkspace = new Workspace({
-      owner: UserId,
-      UserName: UserName,
-      userPass: userPass,
-      members: [UserId],  
+      owner: owner ,
+      wsname: wsname,
+      wspassword: wspassword,
+      members: [owner ],  
       empty: false,
     });
     console.log(newWorkspace)
@@ -22,7 +22,7 @@ let Createworkspace = async (req, res) => {
     res.status(200).json({
       msg: "Workspace created successfully",
       workspaceId: savedWorkspace._id,
-      UserName: savedWorkspace.UserName,
+      wsname: savedWorkspace.wsname,
       members: savedWorkspace.members,
     });
   } catch (err) {
@@ -31,23 +31,23 @@ let Createworkspace = async (req, res) => {
   }
 };
 let joinWorkSpace = async (req, res) => {
-    const { userId, workspaceId, userPass } = req.body;
+    const { owner , workspaceId, wspassword } = req.body;
   
     try {
       const workspace = await Workspace.findById(workspaceId);
       if (!workspace) {
         return res.status(404).json({ msg: "Workspace not found" });
       }
-      if (workspace.userPass !== userPass) {
+      if (workspace.wspassword !== wspassword) {
         return res.status(401).json({ msg: "Invalid workspace password" });
       }
-      if (workspace.members.includes(userId)) {
+      if (workspace.members.includes(owner )) {
         return res.status(400).json({ msg: "User is already a member of the workspace" });
       }
       if (workspace.members.length >= 10) {
         return res.status(400).json({ msg: "Workspace is full" });
       }
-      workspace.members.push(userId);
+      workspace.members.push(owner );
       await workspace.save();
   
       res.status(200).json({
