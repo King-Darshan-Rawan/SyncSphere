6d1d232669886bbba56feb986fa3e7d4731f113b
Aniket Dekate <aniketdekate1@gmail.com>
2024-12-22 15:14:39 +0530

    token

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,17 +1,20 @@
 import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
 
 
 let register = async(req,res)=>{
-    let {firstName, lastName , email , password} = req.body;
-    const username = firstName + " " + lastName;
+    let {firstName, lastName , email , userName, password } = req.body;
+    const fullname = firstName + " " + lastName;
 
     try{
-        let check = await User.findOne({username:username});
+
+        let check = await User.find((user)=> user.userId == userName || user.email == userName);
+
         if(check){
             res.status(400).json({msg:"This username already exist"});
         }else{
-            // console.log("first")
-            const insert_data = await User.insertMany({username:username , email:email , password:password});
+            const hashpass = await bcrypt.hash(password, 10);
+            const insert_data = await User.insertMany({username:username , email:email , password:hashpass , userId:userName});
             res.status(200).json({msg:"User registered" , _id:insert_data[0]._id,username,email});
         }
     }catch(err){
