19081f05077fd3ec3652335e4eaebc3788c5917b
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-12-24 14:45:26 +0530

    with comments

--- backend/controller/chat.js
+++ backend/controller/chat.js
@@ -1,27 +1,29 @@
 import {Chat} from "../models/chat.js";
 
 let accessChat = async(req,res)=>{
-
+//one to one chat 
+//creator id
+// second person id
 }
 
 let fetchChat = async(req,res)=>{
-    
+    // if previous chat exist 
 }
 
 let createGroupChat = async(req,res)=>{
-    
+    // create group
 }
 
 let renameGroup = async(req,res)=>{
-    
+    //rename group
 }
 
 let removeFromGroup = async(req,res)=>{
-    
+    //remove
 }
 
 let addtoGroup = async(req,res)=>{
-    
+    //add
 }
 
 export {accessChat,fetchChat,createGroupChat,renameGroup,removeFromGroup,addtoGroup};
