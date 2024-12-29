ebe6c3ce322e993004c486a67267d033ec193fc4
Darshan Malviya <darshanmalviya9826@gmail.com>
2024-12-29 11:16:16 +0530

    chat

--- backend/controller/chatManager.js
+++ backend/controller/chatManager.js
@@ -1,4 +1,4 @@
-import { Chat } from "../models/chat";
+import { Chat } from "./../models/chat";
 
 // Create one-on-one chat if it doesn't exist
 const createChat = async (req, res) => {
--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,5 +1,5 @@
 
-  import { createChat, fetchChat, createGroupChat, renameGroup, removeFromGroup, addToGroup} from "./../controller/chat"
+  import { createChat, fetchChat, createGroupChat, renameGroup, removeFromGroup, addToGroup} from "../controller/chat"
 
 import express from "express";
 const router = express.Router();
